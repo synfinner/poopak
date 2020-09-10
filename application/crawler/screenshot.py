a3f229e528888485828e6bea5afe9cb15816adf7
synfinner <synfinner@synfinner.ru>
2020-09-10 19:23:03 -0400

    Update screenshot.py

--- application/crawler/screenshot.py
+++ application/crawler/screenshot.py
@@ -2,6 +2,7 @@ from homura import download
 from .config_crawler import get_splash_uri, get_save_path
 
 def get_screenshot(url, filename):
+    # Use try to handle errors with calling the download function. 
     try:
         download(url=get_splash_uri(url),path=get_save_path(filename))
     except:
